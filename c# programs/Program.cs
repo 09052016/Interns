d9a0be8dc75ca24cb1b53fc3c4b8c00b22db51ca
Gopi Chand Reddy <gopichandreddy001@gmail.com>
2024-01-22 11:18:21 +0530

    Initial Commit

---
+++ c# programs/Program.cs
@@ -0,0 +1,69 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace programs{
+    class Temp
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine() + "");
+            try
+            {
+                Convert.ToDouble("12");
+            } 
+            catch(FormatException e) {
+                Console.WriteLine(e.Message);
+            }
+           
+        }
+        public void Matrix(int n)
+        {
+            //Write your code here
+            int[,] matrix = new int[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                int val = -1;
+                for (int j = 0; j < n; j++)
+                {
+                    if (i == j)
+                    {
+                        matrix[i, j] = 0;
+                        val = 1;
+                    }
+                    else
+                    {
+                        matrix[i, j] = val;
+                    }
+
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine("");
+            }
+        }
+        public bool Matrix(int n,int m)
+        {
+            //Write your code here
+            int[,] matrix = new int[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                int val = -1;
+                for (int j = 0; j < n; j++)
+                {
+                    if (i == j)
+                    {
+                        matrix[i, j] = 0;
+                        val = 1;
+                    }
+                    else
+                    {
+                        matrix[i, j] = val;
+                    }
+
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine("");
+            }
+            return true;
+
+        }
+    }
+}
